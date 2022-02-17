da07de8eddaecdedd8d0d7657b5bfcd24b04aa3d
Kitra <kitra>
2022-02-17 05:58:54 +0100

    (init) start and end repository for hive tutorial

---
+++ hive_tutoriel/debut/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:hive_tutoriel/pages/home_page.dart';
+
+void main() {
+  runApp(const App());
+}
+
+// Notre application
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tutoriel Hive',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ hive_tutoriel/fin/android/app/src/main/kotlin/com/example/hive_tutoriel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hive_tutoriel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
